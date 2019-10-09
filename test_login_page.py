29245f92d9928a6ae4def5f5c25c1201f76eb2b9
Michael.co <michael.co@ya.ru>
2019-10-09 10:06:37 +0300

    Correct tests according to lesson

--- test_login_page.py
+++ test_login_page.py
@@ -1,7 +1,7 @@
-from .pages.login_page import LoginPage
-
-def test_guest_can_do_reg_and_login(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+# from .pages.login_page import LoginPage
+#
+# def test_guest_can_do_reg_and_login(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_page()
