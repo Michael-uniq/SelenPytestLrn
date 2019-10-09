b04c8919129c519cb04f493bd1a2a6b75968577a
Michael.co <michael.co@ya.ru>
2019-10-09 23:12:11 +0300

    Create test for product page for lesson

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    MESSAGES_AFTER_CLICK_ON_BASKET = (By.CSS_SELECTOR, '.fade strong')
+    MESSAGE_NAME_PRODUCT_IN_BASKET = (By.CSS_SELECTOR, '.fade strong')
+    PRICE_OF_PRODUCT = (By.CSS_SELECTOR, '.product_main .price_color')
+    NAME_OF_PRODUCT = (By.CSS_SELECTOR, '.product_main h1')
