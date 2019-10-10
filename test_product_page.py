d060f19b35c032ade964ff48bb2b057a78a8e4d0
Michael.co <michael.co@ya.ru>
2019-10-10 09:49:04 +0300

    Rewrite parametrize to clearly code

--- test_product_page.py
+++ test_product_page.py
@@ -2,16 +2,15 @@ import time
 import pytest
 from .pages.product_page import ProductPage
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+xfile = 7
+mask = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
+links = [mask+str(i) for i in range(10) if i != xfile]
+xlink = pytest.param(mask+str(xfile), marks=pytest.mark.xfail(reason="mistake on page"))
+links.insert(xfile, xlink)
+
+
+@pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_basket(browser, link):
     # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
